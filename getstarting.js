6e3a86e628f05b7b0ad3914019201c89800b8a7d
Mohan Muddana <mohanjune@gmail.com>
2013-10-28 03:35:43 +0530

    dynamic generation of nodes and edges

--- getstarting.js
+++ getstarting.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var http = require('http');
 var fs = require('fs');
+var json = require('./graph.json');
 
 var app = express(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = new Buffer(fs.readFileSync('linkedin.html'), 'utf-8');
+  //var buf = new Buffer(fs.readFileSync('d3graph.html'), 'utf-8');
+  var buf = new Buffer(fs.readFileSync('d3graph.html'), 'utf-8');
+  var buf2 = new Buffer(fs.readFileSync('graph.json'), 'utf-8');
   response.send(buf.toString());
 });
 
